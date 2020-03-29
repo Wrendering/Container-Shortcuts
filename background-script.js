3093e32e160d1144bcaca533fc9c71f304759fdf
Wrendering <29991196+Wrendering@users.noreply.github.com>
2020-03-28 22:36:12 -0400

    Old working script

--- background-script.js
+++ background-script.js
@@ -1,32 +1,20 @@
-browser.commands.getAll().forEach( (command) => {
-	let contentToStore = {};
-	contentToStore[command.name] = {
-		"command": command,
-		"cookieStoreId": "",
-		"targetPageHTML": ""
-	};
-	browser.storage.local.set(contentToStore);
-});
-
-browser.commands.onCommand.addListener((commName) => {
-	browser.storage.local.get(commName).then((content) => {
-		browser.tabs.create({
-			url: "about:blank",
-			cookieStoreId: content.cookieStoreId
-		});
-	};
-});
-
 browser.contextualIdentities.query({}).then((identities) => {
 	if (!identities.length) {
 		return;
 	}
 
 	for(let i = 1; i <= 4; i++) {
-		browser.storage.local.get(command
+		let valid = identities.filter( iden => iden.name.includes( i.toString() ) )
 		if( valid.length == 0) break;
 		valid = valid[0];
 
-
+		browser.commands.onCommand.addListener((command) => {
+			if(command.includes(i.toString())) {
+				browser.tabs.create({
+					url: "https://developer.mozilla.org",
+					cookieStoreId: valid.cookieStoreId
+				});	
+			}
+		});
 	}
 });
