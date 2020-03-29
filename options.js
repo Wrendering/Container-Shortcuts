c305f47969d4feb2b6c6f9e2c214c9930c2171db
Wrendering <29991196+Wrendering@users.noreply.github.com>
2020-03-29 01:07:28 -0400

    Shortcut changing works

--- options.js
+++ options.js
@@ -1,18 +1,37 @@
-let table = document.getElementById("Display Table");
+var table = document.getElementById("displayTable");
 
-browser.commands.getAll().then( (commands) => {
-	commands.forEach( (command) => {
-		let commName = command.name;
-		browser.storage.local.get( [commName + "_command", commName + "_cookieStoreId", commName + "_pageHTML" ]).then((content) => {
-			console.log(content);
-			let row = table.insertRow(table.length);
-			let cell_shrct = row.insertCell(0);
-			let cell_cntnr = row.insertCell(1);
+var updateTable = async function() {
+	browser.commands.getAll().then( (commands) => {
+		commands.forEach( (command) => {
+			let commName = command.name;
+			browser.storage.local.get( [commName + "_command", commName + "_cookieStoreId", commName + "_pageHTML" ]).then((content) => {
+				let row = table.insertRow(table.length);
+				row.id = "row_" + commName ;
 
-			let cell_shrct_id = "shrct_cell_" + commName;
-			cell_shrct.innerHTML = "<input type='text' id='" + cell_shrct_id + "' value='" + command.shortcut + "' >";
-			let cell_cntnr_id = "cntnr_cell_" + commName;
-			cell_cntnr.innerHTML = "<label for='" + cell_cntnr_id + "' >" + ( content[commName + "_cookieStoreId"] ? browser.contextualIdentities.get(content[commName + "_cookieStoreId"]).name : "" ) + "</label><input type='text' id='" + cell_cntnr_id + "' >";
+				let cell_shrct = row.insertCell(0);
+				let cell_cntnr = row.insertCell(1);
+	
+				let cell_shrct_id = "shrct_cell_" + commName;
+				cell_shrct.innerHTML = "<input type='text' id='" + cell_shrct_id + "' value='" + command.shortcut + "' >";
+				let cell_cntnr_id = "cntnr_cell_" + commName;
+				cell_cntnr.innerHTML = "<label for='" + cell_cntnr_id + "' >" + ( content[commName + "_cookieStoreId"] ? browser.contextualIdentities.get(content[commName + "_cookieStoreId"]).name : "" ) + "</label><input type='text' id='" + cell_cntnr_id + "' >";
+
+			});
 		});
 	});
-});
+}
+
+document.addEventListener('DOMContentLoaded', updateTable);
+
+
+var updateShortcuts = async function() {
+	for(let i = 1, row; row = table.rows[i]; i++) {
+		console.log(row);
+		browser.commands.update({
+			name: row.id.substring(4),
+			shortcut: row.cells[0].children[0].value
+		});
+	}
+}
+
+document.getElementById('update').addEventListener('click', updateShortcuts);
