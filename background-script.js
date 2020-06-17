f7865872f597d44da5c1947f1c03cc0ee88a9d9a
Wrendering <29991196+Wrendering@users.noreply.github.com>
2020-06-17 17:12:47 -0400

    Catches onCreated/etc and specifically handles onRemoved properly, replacing the container if a key shortcut already has that container. Also, should handle invalid values (if they somehow end up in the db) by opening a default tab. Also, split out some functions in options.js

--- background-script.js
+++ background-script.js
@@ -1,4 +1,4 @@
-browser.storage.local.clear();
+browser.storage.local.clear();		//TODO: This almost certainly shouldn't be here so I preserve data
 
 browser.commands.getAll().then( (commands) => {
 	commands.forEach( (command) => {
@@ -16,21 +16,33 @@ browser.commands.onCommand.addListener( (commName) => {
 		browser.tabs.create({
 			url: "about:blank ",
 			cookieStoreId: content[commName + "_cookieStoreId"]
+		}).catch((err) => {
+			if(err.message.includes("No cookie store exists with ID")) {
+				browser.tabs.create({
+					url: "about:blank "
+				});
+				return;
+			}
 		});
 	});
 });
 
-/*
-var jksldfjksldfjkls = setInterval(() => {
-	browser.runtime.sendMessage("jsdklfj");
-}, 1000);
-*/
 
-/*
+browser.contextualIdentities.onCreated.addListener( async (item) => {
+	browser.runtime.sendMessage("onCreated").catch(err => {});
+});
 
-"options_ui": {
-	"page": "options.html",
-	"browser_style": true
-},
+browser.contextualIdentities.onUpdated.addListener( async (item) => {
+	browser.runtime.sendMessage("onUpdated").catch(err => {});
+});
 
-*/
+browser.contextualIdentities.onRemoved.addListener( async (item) => {
+	const storage = await browser.storage.local.get(null);
+	Object.keys(storage).forEach( async (key) => {
+		if(key.includes("_cookieStoreId") && item.contextualIdentity.cookieStoreId === storage[key]) {
+			let delObj = {}; delObj[key] = "";
+			await browser.storage.local.set(delObj);
+		};
+	});
+	browser.runtime.sendMessage("onRemoved").catch(err => {});
+});
--- popup/options.js
+++ popup/options.js
@@ -1,10 +1,23 @@
-var updateCommandTable = async function() {
+var indexMap = {} ;
+var revIndexMap = {} ;
 
-	let table = document.getElementById("displayTable");
-	table = table.tBodies[0];
+var containerSelectResponse = async function() {
+	let row = this.parentElement.parentElement;
+	let rowI = row.rowIndex;
+	indexMap[revIndexMap[rowI]] = rowI;
+	revIndexMap[rowI] = this.value;
 
-	let indexMap = {} ;
-	let revIndexMap = {} ;
+	let commName = row.id.substring(4);
+	await browser.commands.update({
+		name: commName,
+		shortcut: row.cells[0].children[0].value
+	});
+	let temppp = {} ; temppp[commName + "_cookieStoreId"] = this.value ;
+	await browser.storage.local.set(temppp) ;
+	await browser.storage.local.set({ [commName + "_pageHTML"]: "" });
+};
+
+var constructSelectHTML = async function() {
 	let selectHTML = "<option value=''>Default Tab</option>";
 	await browser.contextualIdentities.query({}).then( (identities) => {
 		let i = 1 ;
@@ -18,39 +31,44 @@ var updateCommandTable = async function() {
 	});
 	selectHTML = "<select>" + selectHTML + "</select>";
 
-	browser.commands.getAll().then( (commands) => {
-		commands.forEach( (command) => {
-			let commName = command.name;
-			browser.storage.local.get( [ commName + "_cookieStoreId", commName + "_pageHTML" ]).then((content) => {
-				let row = table.insertRow(table.length);
-				row.id = "row_" + commName ;
+	return selectHTML;
+}
 
-				let cell_shrct = row.insertCell(0);
-				let cell_cntnr = row.insertCell(1);
+var constructRow = function(newBody, selectHTML, command) {
+	let commName = command.name;
+	browser.storage.local.get( [ commName + "_cookieStoreId", commName + "_pageHTML" ]).then((content) => {
+		let row = newBody.insertRow(newBody.length);
+		row.id = "row_" + commName ;
 
-				let cell_shrct_id = "shrct_cell_" + commName;
-				cell_shrct.innerHTML = "<input type='text' id='" + cell_shrct_id + "' value='" + command.shortcut + "' >";
-				let cell_cntnr_id = "cntnr_cell_" + commName;
-				cell_cntnr.innerHTML = selectHTML;
+		let cell_shrct = row.insertCell(0);
+		let cell_cntnr = row.insertCell(1);
 
-				cell_cntnr.children[0].selectedIndex = indexMap[ content[commName + "_cookieStoreId"] ] ;
-				cell_cntnr.children[0].onchange = async function(){
-					let row = this.parentElement.parentElement;
-					let rowI = row.rowIndex;
-					indexMap[revIndexMap[rowI]] = rowI;
-					revIndexMap[rowI] = this.value;
+		let cell_shrct_id = "shrct_cell_" + commName;
+		cell_shrct.innerHTML = "<input type='text' id='" + cell_shrct_id + "' value='" + command.shortcut + "' >";
+		let cell_cntnr_id = "cntnr_cell_" + commName;
+		cell_cntnr.innerHTML = selectHTML;
 
-					let commName = row.id.substring(4);
-					await browser.commands.update({
-						name: commName,
-						shortcut: row.cells[0].children[0].value
-					});
-					let temppp = {} ; temppp[commName + "_cookieStoreId"] = this.value ;
-					await browser.storage.local.set(temppp) ;
-					await browser.storage.local.set({ [commName + "_pageHTML"]: "" });
-				};
-			});
-		});
+		cell_cntnr.children[0].selectedIndex = indexMap[ content[commName + "_cookieStoreId"] ] ;
+		cell_cntnr.children[0].onchange = containerSelectResponse;
 	});
 }
+
+var updateCommandTable = async function() {
+
+	let table = document.getElementById("displayTable");
+	table = table.tBodies[0];
+	newBody = document.createElement('tbody');
+
+	let selectHTML = await constructSelectHTML();
+
+	browser.commands.getAll().then( (commands) => {
+		commands.forEach( (command) => { constructRow(newBody, selectHTML, command); } );
+	});
+
+	table.parentNode.replaceChild(newBody, table);
+}
 document.addEventListener('DOMContentLoaded', updateCommandTable);
+
+browser.runtime.onMessage.addListener(async (message) => {
+	updateCommandTable();
+});
